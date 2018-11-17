969d59676f0103e1472b1872857ca4aedc122b2b
Ivo Yankulovski <ivoyankulovski@gmail.com>
2018-11-17 11:49:51 +0200

    Update debugform.js
    +mutation observer of zeta elements

--- src/api/js/debugform.js
+++ src/api/js/debugform.js
@@ -1,15 +1,49 @@
+HTMLDocument.prototype.__zetaElements = {};
 HTMLDocument.prototype.registerZetaElement = function(name) {
 	this.getElementsByTagName(name).constructZetaElement(name);
+	this.__zetaElements[name] = {};
+	return this;
 };
-HTMLCollection.prototype.constructZetaElement = function(name) {
-	var un = name.toUpperCase(), l = this.length, i, el;
-	for (i = 0; i < l; i++) {
-		el = this[i];
-		if (el.tagName === un) {
+HTMLDocument.prototype.observeZetaElements = function() {
+	var doc = this, observer = new MutationObserver(function(mutations) {
+		var l = mutations.length, i, an;
+		for (i = 0; i < l; i++) {
+			an = mutations[i].addedNodes;
+			if (an && an.length > 0)
+				doc.constructZetaElements(an);
+		}
+	});
+	observer.observe(this.body, {
+		childList: true,
+		subtree: true
+	});
+	return observer;
+};
+HTMLDocument.prototype.constructZetaElements = function(list) {
+	var zel = this.__zetaElements, name, un, j, el, anl = list.length;
+	for (j = 0; j < anl; j++) {
+		el = list[j];
+		un = el.tagName;
+		name = un ? un.toLowerCase() : null;
+		if (zel[name] && el.constructor === HTMLUnknownElement) {
 			el[un + "_constructor"](name);
 			Object.setPrototypeOf(el, HTMLElement.prototype)
 		}
 	}
+	return this;
+};
+HTMLCollection.prototype.constructZetaElement = function(name, list) {
+	if (!list)
+		list = this;
+	var un = name.toUpperCase(), l = list.length, i, el;
+	for (i = 0; i < l; i++) {
+		el = list[i];
+		if (el.tagName === un && el.constructor === HTMLUnknownElement) {
+			el[un + "_constructor"](name);
+			Object.setPrototypeOf(el, HTMLElement.prototype)
+		}
+	}
+	return this;
 };
 
 HTMLUnknownElement.prototype.FIELD_constructor = function(name) {
@@ -262,7 +296,7 @@ function ResetDebug() {
 }
 
 function onInitBody() {
-	document.registerZetaElement('field');
+	document.registerZetaElement('field').observeZetaElements();
 	initTemplates();
 	initDebugRequestForm();
 	console.log('init body');
